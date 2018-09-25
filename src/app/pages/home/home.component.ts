c948145381c49bd5eaf4d74093143159667386e6
sushil.wis <sushil.webideasolution@gmail.com>
2018-09-25 17:29:49 +0530

    menu items and dashboard

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ScriptLoaderService } from '../../_services/script-loader.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  constructor(private _script: ScriptLoaderService) { }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this._script.load('./assets/js/scripts/dashboard_1_demo.js');
+  }
+
+}
